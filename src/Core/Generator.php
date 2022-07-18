ffc0f087a014c16d3101375bd9c76ebd7ba3145a
Camilo <cemart@gmail.com>
2022-07-18 11:10:48 -0500

    adding changes to upgrade to symfony5

--- src/Core/Generator.php
+++ src/Core/Generator.php
@@ -2,11 +2,11 @@
 namespace Craftsman\Core;
 
 use Craftsman\Core\Command;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
-use \Twig_Loader_Filesystem as Twig_Loader;
-use \Twig_Environment;
+use Symfony\Component\Filesystem\Filesystem;
+use Twig\Environment as Twig_Environment;
+use Twig\Loader\FilesystemLoader as Twig_Loader;
 
 /**
  * Base Generator Class
@@ -29,99 +29,95 @@ abstract class Generator extends Command
      */
     protected $twig;
 
-  	/**
-   	* Class constructor
-   	*/
-  	public function __construct()
-  	{
-      	parent::__construct();
+    /**
+    * Class constructor
+    */
+    public function __construct()
+    {
+        parent::__construct();
 
         $templatesPath = sprintf('%s/utils/templates', rtrim(CRAFTSMANPATH));
 
-      	$this->fs   = new Filesystem();
-      	$this->twig = new Twig_Environment(new Twig_Loader($templatesPath));
-  	}
+        $this->fs   = new Filesystem();
+        $this->twig = new Twig_Environment(new Twig_Loader($templatesPath));
+    }
 
-	  /**
-	   * Command configuration method.
-	   * Configure all the arguments and options.
-	   */
-	  protected function configure()
-	  {
-	      parent::configure();
+    /**
+     * Command configuration method.
+     * Configure all the arguments and options.
+     */
+    protected function configure()
+    {
+        parent::configure();
 
-	      $this
-	      ->addArgument(
-	        'filename',
-	        InputArgument::REQUIRED,
-	        'Generator filename'
-	      )
-	      ->addArgument(
-	        'options',
-	        InputArgument::IS_ARRAY,
-	        'Options passed to all generated files'
-	      )
-	      ->addOption(
-	        'force',
-	        null,
-	        InputOption::VALUE_NONE,
-	        'If set, the task will force the generation process'
-	      );
-	  }
+        $this
+          ->addArgument(
+              'filename',
+              InputArgument::REQUIRED,
+              'Generator filename'
+          )
+          ->addArgument(
+              'options',
+              InputArgument::IS_ARRAY,
+              'Options passed to all generated files'
+          )
+          ->addOption(
+              'force',
+              null,
+              InputOption::VALUE_NONE,
+              'If set, the task will force the generation process'
+          );
+    }
 
-	  /**
-	   * Generate files based on templates.
-	   *
-	   * @param  mixed  $filenames The file to be written to
-	   * @param  mixed  $paths     The Twig_Loader_Filesystem template path
-	   * @param  array  $options   The data to write into the file
-	   * @param  string $template  The template file
-	   * @return bool              Returns true if the file has been created
-	   */
-	  protected function make($filenames, $template = 'base.php.twig', array $options = array())
-	  {
-	      foreach ((array) $filenames as $filename)
-				{
-	          if (! $this->getOption('force') && $this->fs->exists($filename))
-						{
-	              throw new \RuntimeException(sprintf('Cannot duplicate %s', $filename));
-	          }
+    /**
+     * Generate files based on templates.
+     *
+     * @param  mixed  $filenames The file to be written to
+     * @param  mixed  $paths     The Twig_Loader_Filesystem template path
+     * @param  array  $options   The data to write into the file
+     * @param  string $template  The template file
+     * @return bool              Returns true if the file has been created
+     */
+    protected function make($filenames, $template = 'base.php.twig', array $options = array())
+    {
+        foreach ((array) $filenames as $filename) {
+            if (! $this->getOption('force') && $this->fs->exists($filename)) {
+                throw new \RuntimeException(sprintf('Cannot duplicate %s', $filename));
+            }
 
-	          $reflection = new \ReflectionClass(get_class($this));
+            $reflection = new \ReflectionClass(get_class($this));
 
-	          if ($reflection->getShortName() === 'Migration')
-						{
-	              $this->twig->addFunction(new \Twig_SimpleFunction('argument',
-                  function ($field = "") {
-		                return array_combine(array('name','type'), explode(':', $field));
-		              }
+            if ($reflection->getShortName() === 'Migration') {
+                $this->twig->addFunction(new \Twig_SimpleFunction(
+                    'argument',
+                    function ($field = "") {
+                        return array_combine(array('name','type'), explode(':', $field));
+                    }
                 ));
 
-	              foreach ($this->getArgument('options') as $option)
-								{
-	                  list($key, $value) = explode(':', $option);
-	                  $options[$key] = $value;
-	              }
-	          }
+                foreach ($this->getArgument('options') as $option) {
+                    list($key, $value) = explode(':', $option);
+                    $options[$key] = $value;
+                }
+            }
 
             $output = $this->twig->loadTemplate($template)->render($options);
 
-	          $this->fs->dumpFile($filename, $output);
-	      }
-	      return true;
-	  }
+            $this->fs->dumpFile($filename, $output);
+        }
+        return true;
+    }
 
     /**
-	   * Create a directory if doesn't exists.
+       * Create a directory if doesn't exists.
      *
      * @param string $dirPath Directory path
      * @return null
-	   */
+       */
     public function createDirectory($dirPath)
     {
-      if (! $this->fs->exists($dirPath))
-      {
-        $this->fs->mkdir($dirPath);
-      }
+        if (! $this->fs->exists($dirPath)) {
+            $this->fs->mkdir($dirPath);
+        }
     }
 }
